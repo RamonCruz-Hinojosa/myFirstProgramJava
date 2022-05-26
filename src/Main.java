67efac7ed58870c3df4a673504da00e85f44b15b
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2022-05-26 15:28:34 -0500

    25 mins in

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t\\pizzaz");
+        System.out.println("\"pizzazazaz\"");
+        // vid 1 ^^
+
+
+
+
+    }
+
+}
