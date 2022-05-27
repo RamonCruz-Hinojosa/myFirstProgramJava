47a5781896ebebb44585da82b997f7fc3e2606f2
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2022-05-27 15:01:36 -0500

    37:20 mins in

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
+//
+//        System.out.println("\t\\pizzaz");
+//        System.out.println("\"pizzazazaz\"");
+//        // vid 1 ^^
+//        int x = 123; //initialization
+//        double y = 3.14;
+//        boolean z = true;
+//        char symbol = '@';
+//        String name = "Bro";
+//
+//        System.out.println("Hello "+name);
+//        //vid 2^^
 
-        System.out.println("\t\\pizzaz");
-        System.out.println("\"pizzazazaz\"");
-        // vid 1 ^^
-
-
-
+//        String x = "water";
+//        String y = "Kool-Aid";
+//        String temp;
+//
+//        temp = x;
+//        x = y;
+//        y = temp;
+//
+//        System.out.println("x: "+x);
+//        System.out.println("y: "+y);
+          //vid 3 ^^
 
     }
 
